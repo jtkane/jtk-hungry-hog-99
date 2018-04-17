762f45da97af821dd06f353329883c140ffcde49
John T Kane <john@johntkane.com>
2018-04-17 10:32:13 +0100

    Implemented generateRandomString function.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 module.exports = generateRandomString
 
-function generateRandomString() {
-  return 'hungry-hog-99'
+const adjectives = require('adjectives')
+const animals = require('animals')
+const randomNumber = require('random-number-in-range')
+
+function generateRandomString () {
+  return [randomAdjective(), animals(), randomNumber(10, 99)].join('-')
+}
+
+function randomAdjective () {
+  return adjectives[randomNumber(0, adjectives.length - 1)]
 }
